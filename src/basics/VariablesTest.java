4a6cc1e2bc00457c3a5f6ab82aa0bd1919ed7ad0
manupeng <mpeng@colegiovirgendeeuropa.edu.es>
2026-09-10 10:16:11 +0200

    feature: variables study

---
+++ src/basics/VariablesTest.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class VariablesTest {
+	//the structure of the declaration of a variable:
+	//type of the variable + name of the variable;
+	String name = "Nestor";
+	public static void main(String[] args) {
+		System.out.println(1223123);
+	}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module javafundamentalsfp1 {
+}
