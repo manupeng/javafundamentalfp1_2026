1054cfc1aafed0fbe36fbb3e348c66d31f793c12
manupeng <mpeng@colegiovirgendeeuropa.edu.es>
2026-09-11 11:49:39 +0200

    variables and operation

--- src/basics/VariablesTest.java
+++ src/basics/VariablesTest.java
@@ -1,11 +1,44 @@
 package basics;
-
+/**
+ * 
+ * Sep 11, 2026
+ * cepv
+ * What is variable?
+ * Why are there different variables?
+ * What can do with variables?
+ */
 public class VariablesTest {
-	//the structure of the declaration of a variable:
-	//type of the variable + name of the variable;
-	String name = "Nestor";
+	
 	public static void main(String[] args) {
+		
+		//the structure of the declaration of a variable:
+		//type of the variable + name of the variable;
+		// = is the assignment operator
+		String name = "Nestor";
+		System.out.println(name);
 		System.out.println(1223123);
+		System.out.println("Hello Java");
+		name = "Asier";
+		System.out.println(name);
+		//Declaration of an integer variable
+		//an integer has 4 bytes
+		//1 byte is 8 bits
+		int age = 23;
+		long weight = 24l;// a long variable has 8 bytes
+		float height = 23.3f;// a float variable holds 4 bytes
+		double b = 324.34234234;//a double has 8 bytes
+		byte a = -127;//a byte has only one byte of capacity
+		
+		//increase the value of "age" by 10
+		age = age + 10;// + addition operator
+		System.out.println(age);
+		age = age * 10;// * multiplication operator
+		System.out.println(age);
+		weight = weight - 100000;
+		System.out.println(weight);
+		height = height / 100;// division
+		System.out.println(height);
+		
 	}
 	
 }
